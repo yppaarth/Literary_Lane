627ba5e573647dfa2f8ffe3db06afcae363a696c
Pratham2011036 <Ypratham411@gmail.com>
2024-02-16 20:48:18 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,47 @@
+import pickle
+import numpy as np
+from flask import Flask, render_template, request
+
+# Load your data
+popular_df = pickle.load(open('popular.pkl', 'rb'))
+pt = pickle.load(open('pt.pkl', 'rb'))
+books = pickle.load(open('books.pkl', 'rb'))
+similarity_scores = pickle.load(open('similarity_score.pkl', 'rb'))  # Make sure the file name matches exactly
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html',
+                           book_name=list(popular_df['Book-Title'].values),
+                           author=list(popular_df['Book-Author'].values),
+                           image=list(popular_df['Image-URL-M'].values),
+                           votes=list(popular_df['Num-Rating'].values),  # Ensure column name matches your DataFrame
+                           rating=list(popular_df['avg-Rating'].values))  # Ensure column name matches your DataFrame
+
+@app.route('/recommend')
+def recommend_ui():
+    return render_template('recommend.html')
+
+@app.route('/recommend_books', methods=['post'])
+def recommend():
+    user_input = request.form.get('user_input')
+    try:
+        index = np.where(pt.index == user_input)[0][0]
+        similar_items = sorted(list(enumerate(similarity_scores[index])), key=lambda x: x[1], reverse=True)[1:5]
+
+        data = []
+        for i in similar_items:
+            item = []
+            temp_df = books[books['Book-Title'] == pt.index[i[0]]]
+            item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Title'].values))
+            item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Author'].values))
+            item.extend(list(temp_df.drop_duplicates('Book-Title')['Image-URL-M'].values))
+
+            data.append(item)
+
+        return render_template('recommend.html', data=data)
+    except Exception as e:
+        return render_template('recommend.html', error=str(e))
+
+if __name__ == '__main__':
+    app.run(debug=True)
